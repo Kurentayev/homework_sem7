9bf3674383c1abdc31a615d320ccac7a707e755e
Kurentayev <280200@list.ru>
2023-05-05 11:05:28 +0600

    homework sem7

---
+++ task047/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 47: Задайте двумерный массив размером mxn, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+double[,] array = GetArray(m, n);
+PrintArray(array);
+
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+         result[i, j] = Math.Round(new Random().NextDouble() * 100, 2);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+        Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
---
+++ task050/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента 
+// в двумерном массиве, и возвращает значение этого элемента или же указание,
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 1, 7 -> такого числа в массиве нет
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+        result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+        Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Введите первую позицию в двумерном массиве: ");
+int position1 = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите вторую позицию в двумерном массиве: ");
+int position2 = int.Parse(Console.ReadLine() ?? "");
+FindPosition(array, position1, position2, rows, columns);
+
+void FindPosition(int[,] array, int position1, int position2, int rows, int columns)
+{
+    if(position1>(rows-1) || (position2>(columns-1))) 
+    {
+        Console.WriteLine("такого числа в массиве нет"); 
+        return;
+    }
+    Console.WriteLine($"{array[position1, position2]} ");
+}
+
+
